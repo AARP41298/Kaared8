251603dc990814618be98212887ef5e42b76b33a
AARP41298 <AARP41298@users.noreply.github.com>
2019-11-22 17:12:08 -0600

    ka8

--- app/src/main/java/com/example/kaared_v08/MenuActivity.java
+++ app/src/main/java/com/example/kaared_v08/MenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.kaared_v08;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -35,8 +36,13 @@ public class MenuActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                 */
+                Intent intent=new Intent(MenuActivity.this, SemanaActivity.class);
+                startActivity(intent);
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
